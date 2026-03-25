be8fbed6d969e28940c1ba13abdea9b4962c4011
이재현 <corncornwater@mju.ac.kr>
2026-03-25 13:24:30 +0000

    미션3-이재현-완료

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ bucket_list.append(input("맛집 리스트 입력:"));
 bucket_list.append(input("맛집 리스트 입력:"));
 print(f'{"리스트: "}{bucket_list}');
 bucket_list.insert(0,input("맛집 리스트 추가:"));
+print(f'{"리스트: "}{bucket_list}');
 bucket_list.remove(input("도장 깨기:"));
 print(f'{"리스트: "}{bucket_list}');
